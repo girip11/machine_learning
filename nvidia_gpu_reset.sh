da1030078ab7a2953e86a3d7b0ea165a39f09884
girish pasupathy <girishpasupathy@gmail.com>
2021-08-15 11:31:53 +0530

    Changes to be committed: 	modified:   .python-version 	modified:   Pipfile 	modified:   machine_learning.code-workspace 	modified:   nvidia_gpu_reset.sh 	modified:   pyproject.toml 	renamed:    jpnb_qa_check.sh -> run_nbqa.sh 	modified:   setup.cfg

--- nvidia_gpu_reset.sh
+++ nvidia_gpu_reset.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
+
 sudo rmmod nvidia_uvm
 sudo rmmod nvidia_drm
 sudo rmmod nvidia_modeset
 sudo rmmod nvidia
+
 sudo modprobe nvidia
 sudo modprobe nvidia_modeset
 sudo modprobe nvidia_drm
--- run_nbqa.sh
+++ run_nbqa.sh
@@ -7,11 +7,17 @@ if [[ $# -ne 1 ]]; then
   exit
 fi
 
+echo "Running autoflake"
+pipenv run nbqa autoflake -i --remove-all-unused-imports "$1"
+
 echo "Running isort"
 pipenv run nbqa isort "$1"
 
 echo "Running black"
 pipenv run nbqa black "$1"
+
+echo "Running pyupgrade"
+pipenv run pyupgrade "$1"
 
 echo "Running flake8"
 pipenv run nbqa flake8 "$1"
